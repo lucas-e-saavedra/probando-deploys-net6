750d73996f7decda8f3867628d95f79c8c4a066e
tuta1612 <lucassaavedra@yahoo.com.ar>
2022-07-13 22:35:18 -0300

    4to-intento

--- TPLPPA-TPIntegrador/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TPLPPA_TPIntegrador.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
